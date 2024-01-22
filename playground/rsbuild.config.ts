951771724b148af522b9d42fef07085de618dc15
Danielius Kisonas <danieliuskisonas@Danieliuss-MacBook-Pro.local>
2024-01-22 18:45:02 +0200

    revert dev

--- playground/rsbuild.config.ts
+++ playground/rsbuild.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
-import { pluginReactInspector } from '../dist/index';
+import { pluginReactInspector } from 'rsbuild-plugin-react-inspector';
 
 export default defineConfig({
   plugins: [pluginReact(), pluginReactInspector()],
